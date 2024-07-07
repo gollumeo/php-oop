425c3a5d6d0bb3d3e1bc2ec55516b8dd620e222f
gollumeo <p.mauriello54@gmail.com>
2024-07-07 14:40:58 +0200

    fix: choice

--- app/Commands/SetupCommand.php
+++ app/Commands/SetupCommand.php
@@ -45,13 +45,13 @@ class SetupCommand extends Command
         );
         $question->setErrorMessage('%s is not a valid choice.');
         $choice = (int) $helper->ask($input, $output, $question);
-
+        var_dump($choice);
+        exit;
         $this->replaceDirectory();
         mkdir('resources/css', 0777, true);
         mkdir('resources/scss', 0777, true);
 
-            var_dump($choice);
-            exit;
+
         switch ($choice) {
             case 1:
                 $output->writeln("Native CSS initialization...");

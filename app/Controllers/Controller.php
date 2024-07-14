f180635ebaa0a8f1871c4b9e3dcd87e6ac38ccb1
Pierre "Golluméo" Mauriello <p.mauriello54@gmail.com>
2024-07-14 10:30:20 +0200

    Update Controller.php

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -9,6 +9,7 @@ abstract class Controller
     public function view(string $view, array $data = [], $title = null): View
     {
         $viewInstance = new View();
+        extract($data);
         $viewInstance->render($view, $data, $title);
         return $viewInstance;
     }
